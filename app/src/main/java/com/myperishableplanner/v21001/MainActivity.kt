106341f4b5e9816a0cddd0a321c34ebc34727f16
ThapaArchana <thapaan@mail.uc.edu>
2022-02-10 20:51:06 -0500

    Added Input box and and save button

--- app/src/main/java/com/myperishableplanner/v21001/MainActivity.kt
+++ app/src/main/java/com/myperishableplanner/v21001/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.myperishableplanner.v21001
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.myperishableplanner.v21001.ui.theme.MyPerishablePlannerTheme
 
@@ -18,8 +26,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyPerishablePlannerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background , modifier = Modifier.fillMaxWidth()) {
+                    ExpirationFacts("Android")
                 }
             }
         }
@@ -27,15 +35,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ExpirationFacts (name: String) {
+    var itemName by remember { mutableStateOf(value = "")}
+    var category by remember { mutableStateOf(value = "")}
+    var expirationDate by remember { mutableStateOf(value = "")}
+    var description by remember { mutableStateOf(value = "")}
+    val context = LocalContext.current
+
+    Column {
+        OutlinedTextField(
+            value = itemName,
+            onValueChange = { itemName = it },
+            label = { Text(text = stringResource(R.string.itemName)) },
+            modifier = Modifier.fillMaxWidth()
+
+        )
+
+        OutlinedTextField(
+            value = category,
+            onValueChange = { category = it },
+            label = { Text(text = stringResource(R.string.category)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = description,
+            onValueChange = { category = it },
+            label = { Text(text = stringResource(R.string.description)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
+        OutlinedTextField(
+            value = expirationDate,
+            onValueChange = { expirationDate = it },
+            label = { Text(text = stringResource(R.string.expirationDate)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = {
+                Toast.makeText(context, "$itemName $description $category $expirationDate", Toast.LENGTH_LONG)
+                    .show()
+            }){Text(text = "Save")}
+
+    }
+
+
 }
 
-@Preview(showBackground = true)
+@Preview(name="Light mode")
+@Preview(uiMode=Configuration.UI_MODE_NIGHT_YES,showBackground = true, name = "Dark Mode")
 @Composable
 fun DefaultPreview() {
     MyPerishablePlannerTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background , modifier = Modifier.fillMaxWidth()) {
+            ExpirationFacts("Android")
+        }
     }
 }
-//test
