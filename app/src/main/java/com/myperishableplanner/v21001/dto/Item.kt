7eaf2af1f3131967ada454859b8dd415810b52d2
tate <tatettw@live.com>
2022-03-01 15:32:49 -0500

    Item Data class parameters should be var

--- app/src/main/java/com/myperishableplanner/v21001/dto/Item.kt
+++ app/src/main/java/com/myperishableplanner/v21001/dto/Item.kt
@@ -2,4 +2,4 @@ package com.myperishableplanner.v21001.dto
 
 import java.util.*
 
-data class Item(val name: String, val category: String, val expirationDate: Date, val description: String)
+data class Item(var name: String, var category: String, var expirationDate: Date, var description: String)
