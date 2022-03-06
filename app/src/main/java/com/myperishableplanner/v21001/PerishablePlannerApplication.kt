b0fca263c9236bc364faa9445dcf36a9ce6ef885
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-03-06 14:54:56 -0500

    Create PerishablePlannerApplication.kt

---
+++ app/src/main/java/com/myperishableplanner/v21001/PerishablePlannerApplication.kt
@@ -0,0 +1,8 @@
+package com.myperishableplanner.v21001
+import android.app.Application
+
+class PerishablePlannerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
