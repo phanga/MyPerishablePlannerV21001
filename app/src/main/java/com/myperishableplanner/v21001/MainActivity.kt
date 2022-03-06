604862432f738d139dc7d50359783be66f2a1185
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-03-06 14:54:53 -0500

    Update MainActivity.kt

--- app/src/main/java/com/myperishableplanner/v21001/MainActivity.kt
+++ app/src/main/java/com/myperishableplanner/v21001/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
