fe3c8eb1e7f2e6780ad1d72f29aa63a622ec884e
Yanling <zentgryg@mail.uc.edu>
2022-03-06 20:38:29 -0500

    add Item data class

--- app/src/main/java/com/myperishableplanner/v21001/dto/Item.kt
+++ app/src/main/java/com/myperishableplanner/v21001/dto/Item.kt
@@ -1,3 +1,9 @@
 package com.myperishableplanner.v21001.dto
 
-data class Item()
+import java.util.*
+
+data class Item(var itemName: String, var category: String, var expirationDate: Date, var description: String, var id: Int =0){
+    override fun toString(): String {
+        return "$itemName $category $expirationDate $description"
+    }
+}
