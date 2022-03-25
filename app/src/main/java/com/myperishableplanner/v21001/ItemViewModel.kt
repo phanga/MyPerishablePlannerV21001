71ec3cb8a726f1e7c140b1f782f9c332caf7cb3c
kirkai <kirkai@mail.uc.edu>
2022-03-25 11:42:34 -0400

    Implementing Firebase start

--- app/src/main/java/com/myperishableplanner/v21001/ItemViewModel.kt
+++ app/src/main/java/com/myperishableplanner/v21001/ItemViewModel.kt
@@ -7,9 +7,17 @@ import com.myperishableplanner.v21001.dto.Item
 import com.myperishableplanner.v21001.service.IItemService
 import com.myperishableplanner.v21001.service.ItemService
 import kotlinx.coroutines.launch
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+
 
 class ItemViewModel (var itemService: IItemService = ItemService()): ViewModel() {
+
+
         var items : MutableLiveData<List<Item>> = MutableLiveData<List<Item>>()
+
+        private lateinit var firestore : FirebaseFirestore
+
         fun fetchItems(){
             viewModelScope.launch {
                 items.postValue(itemService.fetchItems())
