bd2786287d60f5e6d3fdf382902676bfcafc15fa
kirkai <kirkai@mail.uc.edu>
2022-03-25 11:56:38 -0400

    Fixed firebase with added dependencies

--- app/src/main/java/com/myperishableplanner/v21001/ItemViewModel.kt
+++ app/src/main/java/com/myperishableplanner/v21001/ItemViewModel.kt
@@ -7,8 +7,9 @@ import com.myperishableplanner.v21001.dto.Item
 import com.myperishableplanner.v21001.service.IItemService
 import com.myperishableplanner.v21001.service.ItemService
 import kotlinx.coroutines.launch
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
+
 
 
 class ItemViewModel (var itemService: IItemService = ItemService()): ViewModel() {
